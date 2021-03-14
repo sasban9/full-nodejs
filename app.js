9d9661e263ad49ff737cec7ec5af8dee5882793b
sasban <azure.saswata@gmail.com>
2021-03-14 17:39:59 +0530

    72 Using a Helper Function for Navigation

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, '.', 'favicon.ico'));
+})
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
