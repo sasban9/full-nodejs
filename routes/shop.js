01dba5fcfe2fb6f5f771c1a87458e3a2ec8085c2
sasban <azure.saswata@gmail.com>
2021-03-24 13:47:21 +0530

    81 Outputting Dynamic Content

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     // console.log('shop.js', adminData.products);
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    res.render('shop');
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop'});
 })
 
 
