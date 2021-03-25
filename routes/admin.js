9b8a12b2240b1733551275fcfd21c96a9208b0e5
sasban <azure.saswata@gmail.com>
2021-03-25 13:03:40 +0530

    89 Adding the layout to handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const products = [];
 
 router.get('/add-product', (req, res, next) => {
     //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));    
-    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product' });
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
 })
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get('/', (req, res, next) => {
     // console.log('shop.js', adminData.products);
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0});
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, productCSS: true, activeShop: true});
 })
 
 
