22f5fbc1a086164cceb707d06aabbb5f961cc0fa
sasban <azure.saswata@gmail.com>
2022-10-11 22:50:24 +0530

    107 Creating the Shop Structure

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formsCSS: true,
@@ -23,7 +23,7 @@ exports.getProducts = (req, res, next) => {
   // console.log('shop.js', adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   Product.fetchAll(products => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
       path: "/",
