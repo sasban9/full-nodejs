57b93a7ce6165c1091ff82738a2be52a1345c003
sasban <azure.saswata@gmail.com>
2022-10-11 23:13:54 +0530

    109 Registering the Routes

---
+++ controllers/admin.js
@@ -0,0 +1,29 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render("admin/add-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true,
+    });
+  };
+  
+exports.postAddProduct = (req, res, next) => {
+    // products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect("/");
+  };
+  
+  exports.getProducts = (req, res, next) => {
+    Product.fetchAll(products => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Productss",
+        path: '/admin/products'
+      });
+    });
+  };
