e8df575d3943c141fa954812658ebaea5ceaa67d
sasban <azure.saswata@gmail.com>
2022-10-11 21:11:30 +0530

    103 Refactoring the File Storage Code

--- models/product.js
+++ models/product.js
@@ -2,6 +2,16 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+}
 
 module.exports = class Product {
     constructor(t) {
@@ -9,28 +19,16 @@ module.exports = class Product {
     }
 
     save() {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            // console.log(fileContent);
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
+        // const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
-        })
+        });
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
-        // return products;
+        getProductsFromFile(cb);
     }
 }
