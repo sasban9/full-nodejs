84f841cc1235f3c6b334b9fd6ffa9ff7b8f505ba
sasban <azure.saswata@gmail.com>
2022-10-12 18:06:05 +0530

    127 Editing the Product Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,7 +16,7 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect("/");
   };
@@ -32,12 +32,29 @@ exports.getEditProduct = (req, res, next) => {
       return res.redirect('/');
     }
     res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
+      pageTitle: "Edit Product :: "+product.title,
       path: "/admin/edit-product",
       editing: editMode,
       product: product
     });
   });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
