b6457c0fde5711313cd336b9a3367729e7b9e1d4
sasban <azure.saswata@gmail.com>
2022-10-13 14:47:41 +0530

    156 Retrieving data & finding Products

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,16 +3,15 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/index", {
-        prods: rows,
-        pageTitle: "Shop",
-        path: '/products'
+        prods: products,
+        pageTitle: "All Products",
+        path: "/products",
       });
     })
     .catch((err) => console.log(err));
-  
 };
 
 exports.getProduct = (req, res, next) => {
@@ -29,16 +28,15 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/index", {
-        prods: rows,
+        prods: products,
         pageTitle: "Shop",
-        path: '/'
+        path: "/",
       });
     })
     .catch((err) => console.log(err));
-  
 };
 
 exports.getCart = (req, res, next) => {
