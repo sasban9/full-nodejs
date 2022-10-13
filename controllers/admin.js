df86f73f5e4937abe1f3704b5e8f036f38774f62
sasban <azure.saswata@gmail.com>
2022-10-13 15:19:49 +0530

    159 Updating Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -28,21 +28,23 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  if(!editMode) {
-    res.redirect('/');
+  if (!editMode) {
+    return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId, product => {
-    if(!product) {
-      return res.redirect('/');
+  Product.findByPk(prodId)
+  .then(product => {
+    if (!product) {
+      return res.redirect("/");
     }
     res.render("admin/edit-product", {
-      pageTitle: "Edit Product :: "+product.title,
+      pageTitle: "Edit Product :: " + product.title,
       path: "/admin/edit-product",
       editing: editMode,
-      product: product
+      product: product,
     });
-  });
+  })
+  .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -51,15 +53,18 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(prodId).then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc;
+    return product.save();
+  })
+  .then(result => {
+    console.log('UPDATED PRODUCT!');
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
