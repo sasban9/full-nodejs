3f7d61f2d96574bc31bdb4c6d2c3c545f022afa6
sasban <azure.saswata@gmail.com>
2022-10-13 14:30:28 +0530

    152 Defining a Model

--- util/database.js
+++ util/database.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: ''
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: ''
+// });
+
+// module.exports = pool.promise();
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node-complete", "root", "", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
