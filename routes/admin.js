ed643d3d73469a7542002906fa70e8a564c9a5d4
sasban <azure.saswata@gmail.com>
2022-10-14 23:45:01 +0530

    190 Finishing update product

--- routes/admin.js
+++ routes/admin.js
@@ -8,11 +8,11 @@ const adminController = require('../controllers/admin');
 const router = express.Router();
 
 router.get('/add-product', adminController.getAddProduct);
-// router.get('/products', adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 router.post('/add-product', adminController.postAddProduct);
-// router.get('/edit-product/:productId', adminController.getEditProduct);
-// router.post('/edit-product',adminController.postEditProduct);
-// router.post('/delete-product',adminController.postEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
+router.post('/edit-product',adminController.postEditProduct);
+router.post('/delete-product',adminController.postEditProduct);
 
 module.exports = router;
