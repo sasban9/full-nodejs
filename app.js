54fbc471c95f82639ec790fa9658160bc16c27e8
sasban <azure.saswata@gmail.com>
2022-10-14 22:59:56 +0530

    184 Creating Product

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 //const expressHbs = require('express-handlebars');
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database");
+const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
@@ -13,7 +13,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const adminRoutes = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 // const shopRoutes = require("./routes/shop");
 
 //app.use(bodyParser.urlencoded({extended:true}));
@@ -27,9 +27,10 @@ app.use((req, res, next) => {
   //     next();
   //   })
   //   .catch((err) => console.log(err));
+  next();
 });
 
-// app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
--- models/product.js
+++ models/product.js
@@ -1,7 +1,4 @@
 const getDb = require('../util/database').getDb;
-// const Sequelize = require('sequelize');
-
-// const sequelize = require('../util/database');
 
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -12,30 +9,12 @@ class Product {
   }
 
   save() {
-    
+    const db = getDb();
+    return db.collection("products")
+      .insertOne(this)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
   }
 }
-
-const Product = sequelize.define('product', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    title: Sequelize.STRING,
-    price: {
-        type: Sequelize.DOUBLE,
-        allowNull: false
-    },
-    imageUrl: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    description: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-});
 
 module.exports = Product;
