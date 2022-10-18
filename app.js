4ee3b72bc16a09c513504553e27a75a61714778f
sasban <azure.saswata@gmail.com>
2022-10-18 21:02:51 +0530

    218 Adding and Using a User Model

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('634afe2b1798710f7b4518f5')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('634ec5c88ef045b17dd0c4ac')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -38,6 +38,19 @@ mongoose
     "mongodb+srv://sasUser:5jqesqyPEEHGnNIC@cluster0.bopqy.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
+    User.findOne().then(user => {
+      if(!user) {
+        const user = new User({
+          name: "Saswata",
+          email: "saswata@styched.in",
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    
     app.listen(3000);
   })
   .catch((err) => console.log(err));
