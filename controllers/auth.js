80e33a1df6fd5fccc253478651844b529f54c8fb
sasban <azure.saswata@gmail.com>
2022-10-20 22:10:18 +0530

    257 Adding SignIn

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,18 +20,33 @@ exports.getSignup = (req, res, next) => {
 };
       
 exports.postLogin = (req, res, next) => {
-    //   res.setHeader("Set-Cookie", "loggedIn=true");
-    User.findById("634ef2f1e1db969f520e401d")
-      .then((user) => {
-        req.session.isLoggedIn = true;
-        req.session.user = user;
-        req.session.save(err => {
-          console.log(err);
-          res.redirect("/");
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if(doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
         })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/login");
+        });
       })
-      .catch((err) => console.log(err));
-  };
+    .catch((err) => console.log(err));
+};
 
   exports.postSignup = (req, res, next) => {
     const email = req.body.email;
