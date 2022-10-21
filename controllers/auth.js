765fded0dc2acc3bbce524961386f1dbd8b10523
sasban <azure.saswata@gmail.com>
2022-10-21 16:55:57 +0530

    278 Implementing Token Logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
@@ -129,5 +131,37 @@ exports.getReset = (req, res, next) => {
     path: "/reset",
     pageTitle: "Reset Password",
     errorMessage: message
+  });
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if(err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({email: req.body.email}).then(user =>{
+      if(!user) {
+        req.flash('error', 'No account found with that email.');
+        return res.redirect('/reset');
+      }
+      user.resetToken = token;
+      user.resetTokenExpiration = Date.now() + 3600000;
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/');
+      transporter.sendMail({
+        to: req.body.email,
+        from: 'saswata@styched.in',
+        subject: 'Password Reset',
+        html: `
+          <h3>You requested a password reset!</h3>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+        `
+      });
+    })
+    .catch(err => {console.log(err)});
   });
 }
--- routes/auth.js
+++ routes/auth.js
@@ -11,5 +11,6 @@ router.get('/reset', authController.getReset);
 router.post('/login', authController.postLogin);
 router.post('/signup', authController.postSignup);
 router.post('/logout', authController.postLogout);
+router.post('/reset', authController.postReset);
 
 module.exports = router;
