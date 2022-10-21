920344f1843ea0d174261399bbc8514624c84d31
sasban <azure.saswata@gmail.com>
2022-10-21 23:13:35 +0530

    280 Creating the Reset Password Form

--- controllers/auth.js
+++ controllers/auth.js
@@ -151,7 +151,7 @@ exports.postReset = (req, res, next) => {
       return user.save();
     })
     .then(result => {
-      res.redirect('/');
+      // res.redirect('/');
       transporter.sendMail({
         to: req.body.email,
         from: 'saswata@styched.in',
@@ -165,3 +165,23 @@ exports.postReset = (req, res, next) => {
     .catch(err => {console.log(err)});
   });
 }
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "Reset Password",
+        errorMessage: message,
+        userId: user._id.toString() 
+      });
+    })
+    .catch((err) => console.log(err));
+};
