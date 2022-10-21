d9cd2fd962e01056c5b643622526981cd4e126bf
sasban <azure.saswata@gmail.com>
2022-10-21 23:29:00 +0530

    283 Adding Authorization

--- routes/auth.js
+++ routes/auth.js
@@ -13,5 +13,6 @@ router.post('/login', authController.postLogin);
 router.post('/signup', authController.postSignup);
 router.post('/logout', authController.postLogout);
 router.post('/reset', authController.postReset);
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
