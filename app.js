8cb2961c193462c79306e1d5e2604304cadb08f9
sasban <azure.saswata@gmail.com>
2022-10-28 22:16:21 +0530

    322 Configuring multer to adjust filename & filepath

--- app.js
+++ app.js
@@ -1,43 +1,52 @@
-const path = require('path');
+const path = require("path");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf');
-const flash = require('connect-flash');
-const multer = require('multer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const multer = require("multer");
 
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
 // const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const User = require("./models/user");
 
 const app = express();
 const store = new MongoDBStore({
   uri: process.env.MONGODB_URI,
-  collection: 'sessions'
+  collection: "sessions",
 });
 const csrfProtection = csrf();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "media");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({dest: 'media'}).single('image'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer({ storage: fileStorage }).single("image"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
     secret: "my secret",
     resave: false,
     saveUninitialized: false,
-    store: store
+    store: store,
   })
 );
 app.use(csrfProtection);
@@ -51,37 +60,37 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   // throw new Error('Sync Dummy!');
-  if(!req.session.user){
+  if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
-  .then((user) => {
-    // throw new Error('Dummy!');
-    if(!user)return next();
-    req.user = user;
-    next();
-  })
-  .catch((err) => {
-    // throw new Error(err);
-    next(new Error(err));
-  });
+    .then((user) => {
+      // throw new Error('Dummy!');
+      if (!user) return next();
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      // throw new Error(err);
+      next(new Error(err));
+    });
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.get('/500', errorController.get500);
+app.get("/500", errorController.get500);
 
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render(...);
   // res.redirect('/500');
-  res.status(500).render('500', {
-    pageTitle: 'Error!',
-    path: '/500',
-    isAuthenticated: req.session.isLoggedIn
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
   });
 });
 
